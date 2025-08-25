39287178d7db608bda72c71a266fb1dea7fdff4e
Kevin Bralten <kevin@bralten.ca>
2025-08-25 19:39:12 -0400

    Fix batch script condition to handle spaces.

--- run_stl_to_depthmap.bat
+++ run_stl_to_depthmap.bat
@@ -2,7 +2,7 @@
 REM Batch script to run stl_to_depthmap.py with the correct Python environment
 REM Usage: run_stl_to_depthmap.bat <stl_file> [additional_options]
 
-if "%1"=="" (
+if %1=="" (
     echo Usage: %0 ^<stl_file^> [additional_options]
     echo Example: %0 foam.stl --verbose
     exit /b 1
